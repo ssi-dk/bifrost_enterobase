60489481daee87c0396f0628007bda90966db6bb
Kristoffer Kiil <krki@ssi.dk>
2021-11-29 11:40:31 +0100

    Bump version: v1_1_1__ → v1_1_2__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bifrost_enterobase',
-    version='v1_1_1__',
+    version='v1_1_2__',
     description='Enterobase component for salmonella serotyping',
     url='https://github.com/ssi-dk/bifrost_enterobase',
     author="Kristoffer Kiil",
